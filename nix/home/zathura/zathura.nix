5c3a056305b1c505e7db00ceac036aa0fc672fbe
Andrieee44 <andrieee44@gmail.com>
2023-05-05 23:41:14 +0800

    [zathura] Set up options and mappings

---
+++ nix/home/zathura/zathura.nix
@@ -0,0 +1,25 @@
+{ config, ... }:
+{
+	config.programs.zathura = {
+		options = {
+			sandbox = "normal";
+			statusbar-h-padding = 0;
+			statusbar-v-padding = 0;
+			page-padding = 1;
+			selection-clipboard = "clipboard";
+		};
+
+		mappings = {
+			u = "scroll half-up";
+			d = "scroll half-down";
+			D = "toggle_page_mode";
+			r = "reload";
+			R = "rotate";
+			K = "zoom in";
+			J = "zoom out";
+			i = "recolor";
+			p = "print";
+			g = "goto top";
+		};
+	};
+}
