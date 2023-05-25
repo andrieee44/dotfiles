7f43bf51845b34a04aabf4d997dd5e0e4aa32ed6
Andrieee44 <andrieee44@gmail.com>
2023-05-25 18:44:59 +0800

    texlive: add latexmkrc

--- nix/home/texlive/texlive.nix
+++ nix/home/texlive/texlive.nix
@@ -1,9 +1,22 @@
-{ config, ... }:
+{ config, lib, ... }:
 {
-	config.programs.texlive = {
-		extraPackages = tpkgs:
-		{
-			inherit (tpkgs) scheme-full;
+	config = {
+		programs.texlive = {
+			extraPackages = tpkgs:
+			{
+				inherit (tpkgs) scheme-full;
+			};
+		};
+
+		home.file.latexmkrc = lib.mkIf config.programs.texlive.enable {
+			executable = false;
+			target = "${config.xdg.configHome}/latexmk/latexmkrc";
+
+			text = ''
+				$ENV{'TEXMFHOME'} = "${config.xdg.dataHome}/texlive";
+				$ENV{'TEXMFCONFIG'} = "${config.xdg.configHome}/texlive";
+				$ENV{'TEXMFVAR'} = "${config.xdg.cacheHome}/texlive";
+			'';
 		};
 	};
 }
