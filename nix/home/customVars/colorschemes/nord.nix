49f807a4c79b5b46417f7f34463281ef086cd76d
andrieee44 <andrieee44@gmail.com>
2023-08-18 21:00:43 +0800

    colorschemes: refactor fzf

--- nix/home/customVars/colorschemes/nord.nix
+++ nix/home/customVars/colorschemes/nord.nix
@@ -54,6 +54,28 @@ in {
 				normal = colorscheme.normal;
 				bright = colorscheme.bright;
 			};
+
+			fzf.colors = {
+				fg = "7";
+				preview-fg = "7";
+				preview-bg = "0";
+				hl = "4";
+				"fg+" = "7";
+				"bg+" = "0";
+				gutter = "0";
+				"hl+" = "4";
+				query = "7";
+				disabled = "8";
+				info = "6";
+				separator = "6";
+				border = "6";
+				spinner = "6";
+				label = "6";
+				prompt = "6";
+				pointer = "6";
+				marker = "6";
+				header = "6";
+			};
 		};
 
 		xdg.configFile.dircolors = {
--- nix/home/fzf/fzf.nix
+++ nix/home/fzf/fzf.nix
@@ -11,31 +11,5 @@
 			"--no-mouse"
 			"--preview-window border-left,wrap"
 		];
-
-		colors = let
-			colorschemes = {
-				nord = {
-					fg = "7";
-					preview-fg = "7";
-					preview-bg = "0";
-					hl = "4";
-					"fg+" = "7";
-					"bg+" = "0";
-					gutter = "0";
-					"hl+" = "4";
-					query = "7";
-					disabled = "8";
-					info = "6";
-					separator = "6";
-					border = "6";
-					spinner = "6";
-					label = "6";
-					prompt = "6";
-					pointer = "6";
-					marker = "6";
-					header = "6";
-				};
-			};
-		in colorschemes.${config.customVars.colorscheme};
 	};
 }
