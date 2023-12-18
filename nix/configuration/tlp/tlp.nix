29350272fa8038aa68f77175bb7f461d2a25664f
andrieee44 <andrieee44@gmail.com>
2023-12-18 20:10:06 +0800

    tlp: enable performance while in AC

--- nix/configuration/tlp/tlp.nix
+++ nix/configuration/tlp/tlp.nix
@@ -2,13 +2,16 @@
 {
 	config.services.tlp = {
 		settings = {
-			STOP_CHARGE_THRESH_BAT0 = 1;
-			RESTORE_THRESHOLDS_ON_BAT = 1;
 			BAY_POWEROFF_ON_AC = 1;
 			BAY_POWEROFF_ON_BAT = 1;
+			CPU_ENERGY_PERC_POLICY_ON_AC = "performance";
+			CPU_ENERGY_PERC_POLICY_ON_BAT = "power";
+			CPU_SCALING_GOVERNOR_ON_AC = "performance";
+			CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 			DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth wwan";
-			SOUND_POWER_SAVE_ON_AC = 10;
+			RESTORE_THRESHOLDS_ON_BAT = 1;
 			SOUND_POWER_SAVE_ON_BAT = 10;
+			STOP_CHARGE_THRESH_BAT0 = 1;
 		};
 	};
 }
