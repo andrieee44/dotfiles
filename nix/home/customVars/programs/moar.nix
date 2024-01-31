8a658898d9dc186a6ffb065a3eb1143b69ba66c3
andrieee44 <andrieee44@gmail.com>
2024-01-31 21:27:05 +0800

    moar: move option values to top of implementation

--- nix/home/customVars/programs/moar.nix
+++ nix/home/customVars/programs/moar.nix
@@ -1,28 +1,30 @@
 { config, pkgs, lib, ... }:
 {
-	options.customVars.programs.moar = {
-		enable = lib.mkEnableOption "";
-		pager = lib.mkEnableOption "";
+	options.customVars.programs.moar = let
+		mkEnableOption = config.customVars.mkOption lib.types.bool;
+	in {
+		enable = mkEnableOption;
+		pager = mkEnableOption;
 		style = config.customVars.mkOption lib.types.str;
 	};
 
 	config = let
 		cfg = config.customVars.programs.moar;
 	in {
-		home = lib.mkIf cfg.enable {
-			packages = [ pkgs.moar ];
-			sessionVariables = {
-				PAGER = lib.mkIf cfg.pager "${pkgs.moar}/bin/moar";
-				MOAR = "--style ${cfg.style}";
-			};
-		};
-
 		customVars.programs.moar = {
 			enable = true;
 			pager = true;
 			style = let
 				colorscheme = config.customVars.colorscheme;
 			in if colorscheme == "default" then "native" else colorscheme;
+		};
+
+		home = lib.mkIf cfg.enable {
+			packages = [ pkgs.moar ];
+			sessionVariables = {
+				PAGER = lib.mkIf cfg.pager "${pkgs.moar}/bin/moar";
+				MOAR = "--style ${cfg.style}";
+			};
 		};
 	};
 }
