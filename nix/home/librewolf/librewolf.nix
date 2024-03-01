72f89f43e7bf7379a7c6a3bbf056ac681a96c8b9
andrieee44 <andrieee44@gmail.com>
2024-03-01 21:51:30 +0800

    librewolf: delete history on startup

--- nix/home/librewolf/librewolf.nix
+++ nix/home/librewolf/librewolf.nix
@@ -1,15 +1,15 @@
 { ... }:
 {
 	config.programs.librewolf.settings = {
-		"privacy.clearOnShutdown.history" = false;
-		"privacy.clearOnShutdown.downloads" = false;
+		"privacy.clearOnShutdown.history" = true;
+		"privacy.clearOnShutdown.downloads" = true;
 		"privacy.history.custom" = false;
 		"privacy.donottrackheader.enabled" = true;
-		"browser.startup.couldRestoreSession.count" = 1;
-		"browser.search.suggest.enabled" = true;
-		"browser.urlbar.suggest.searches" = true;
+		"browser.startup.couldRestoreSession.count" = 0;
+		"browser.search.suggest.enabled" = false;
+		"browser.urlbar.suggest.searches" = false;
 		"intl.regional_prefs.use_os_locales" = true;
 		"services.sync.engine.addresses.available" = false;
-		"toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+		"toolkit.legacyUserProfileCustomizations.stylesheets" = false;
 	};
 }
