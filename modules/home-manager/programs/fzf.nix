032c68c381515e63e1947ae85dd1c94993f259bd
andrieee44 <andrieee44@gmail.com>
2024-03-17 21:01:25 +0800

    fzf: add colors

--- modules/home-manager/programs/fzf.nix
+++ modules/home-manager/programs/fzf.nix
@@ -1,6 +1,21 @@
-{ ... }:
+{ colorscheme, ... }:
 {
 	programs.fzf = {
+		colors = {
+			fg = "#${colorscheme.palette.base04}";
+			bg = "#${colorscheme.palette.base00}";
+			hl = "#${colorscheme.palette.base0D}";
+			"fg+" = "#${colorscheme.palette.base06}";
+			"bg+" = "#${colorscheme.palette.base01}";
+			info = "#${colorscheme.palette.base0A}";
+			border = "#${colorscheme.palette.base0D}";
+			prompt = "#${colorscheme.palette.base0A}";
+			pointer = "#${colorscheme.palette.base0C}";
+			marker = "#${colorscheme.palette.base0C}";
+			spinner = "#${colorscheme.palette.base0C}";
+			header = "#${colorscheme.palette.base0D}";
+		};
+
 		defaultOptions = [
 			"--exact"
 			"--reverse"
