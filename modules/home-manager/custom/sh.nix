319304db32e1b642e7b9e90d8e027aeebd72a6f3
andrieee44 <andrieee44@gmail.com>
2024-03-17 14:36:45 +0800

    sh: fix fzf shell scripts

--- modules/home-manager/custom/sh.nix
+++ modules/home-manager/custom/sh.nix
@@ -21,7 +21,7 @@
 				${pkgs.toybox}/bin/find -L "$d" -mindepth 1 -maxdepth 1 -type f -perm -u=x -not -name '.*' -printf "%f\n" >& 3
 			done
 
-			${pkgs.toybox}/bin/sort <& 4 | ${pkgs.fzf}/bin/fzf-tmux --header "Search executable:"
+			${pkgs.toybox}/bin/sort <& 4 | ${pkgs.fzf}/bin/fzf-tmux --header "Search executable:" -p 45%,60% --border=sharp
 			exec 3>& -
 		'';
 
@@ -40,7 +40,7 @@
 					gsub("^[[:space:]]*", "", $1)
 					print($1)
 				}
-			' | ${pkgs.fzf}/bin/fzf-tmux --header "System action:")"
+			' | ${pkgs.fzf}/bin/fzf-tmux --header "System action:" -p 45%,60% --border=sharp)"
 
 			err="$?"
 			[ "$err" -ne 0 ] && exit "$err"
--- modules/home-manager/programs/fzf.nix
+++ modules/home-manager/programs/fzf.nix
@@ -1,14 +1,6 @@
 { ... }:
 {
 	programs.fzf = {
-		tmux = {
-			enableShellIntegration = true;
-			shellIntegrationOptions = [
-				"-p '45%,60%'"
-				"--border=sharp"
-			];
-		};
-
 		defaultOptions = [
 			"--exact"
 			"--reverse"
