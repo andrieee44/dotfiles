fb36a9cac50bef7534a5493f51dd69d3bd5398e9
andrieee44 <andrieee44@gmail.com>
2024-04-10 10:59:37 +0800

    starship: add status

--- modules/home-manager/programs/starship/starship.nix
+++ modules/home-manager/programs/starship/starship.nix
@@ -1,25 +1,22 @@
-{ config, pkgs, lib, colorscheme, ... }:
+{ config, lib, ... }:
 {
 	xdg.configFile = let
 		base = config.xdg.configFile."starship.toml".source;
 	in {
-		"starship.toml".target = "starship/base.toml";
-		"starship/gui.toml".text = (builtins.readFile base) + (builtins.readFile ./gui.toml);
-		"starship/tty.toml".text = (builtins.readFile base) + (builtins.readFile ./tty.toml);
-	};
+		"starship/gui.toml".text = builtins.readFile base + builtins.readFile ./gui.toml;
+		"starship/tty.toml".text = builtins.readFile base + builtins.readFile ./tty.toml;
 
-	programs = let
-		cfg = config.programs.starship;
-	in {
-
-		zsh.initExtra = lib.mkIf cfg.enableZshIntegration ''
-			[ "$XDG_SESSION_TYPE" = "tty" ] && \
-				export STARSHIP_CONFIG="${config.home.homeDirectory}/${config.xdg.configFile."starship/tty.toml".target}" || \
-				export STARSHIP_CONFIG="${config.home.homeDirectory}/${config.xdg.configFile."starship/gui.toml".target}"
-		'';
-
-		starship.settings = {
-			add_newline = false;
+		"starship.toml" = {
+			source = ./base.toml;
+			target = "starship/base.toml";
 		};
 	};
+
+	programs.zsh.initExtra = let
+		cfg = config.programs.starship;
+	in lib.mkIf cfg.enableZshIntegration ''
+		[ "$XDG_SESSION_TYPE" = "tty" ] && \
+			export STARSHIP_CONFIG="${config.home.homeDirectory}/${config.xdg.configFile."starship/tty.toml".target}" || \
+			export STARSHIP_CONFIG="${config.home.homeDirectory}/${config.xdg.configFile."starship/gui.toml".target}"
+	'';
 }
