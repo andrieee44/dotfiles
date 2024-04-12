f64decb60987e2a19567f1445c20ede770a788d3
andrieee44 <andrieee44@gmail.com>
2024-04-12 23:25:11 +0800

    imv: add colors

--- modules/home-manager/programs/imv.nix
+++ modules/home-manager/programs/imv.nix
@@ -1,10 +1,14 @@
-{ config, ... }:
+{ config, colorscheme, ... }:
 {
 	programs.imv.settings = {
 		options = {
 			overlay = true;
 			overlay_font = "${config.gtk.font.name}:${builtins.toString config.gtk.font.size}";
 			overlay_position_bottom = true;
+
+			overlay_text_color = colorscheme.palette.base05;
+			overlay_background_color = colorscheme.palette.base00;
+			background = colorscheme.palette.base03;
 		};
 
 		binds = {
