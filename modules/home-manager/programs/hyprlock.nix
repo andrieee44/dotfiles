d13cc9ffdec3ce0b85a5826a139970b7c6e0e072
andrieee44 <andrieee44@gmail.com>
2024-04-17 23:01:49 +0800

    hyprlock: add lockscreen wallpaper

--- modules/home-manager/programs/hyprlock.nix
+++ modules/home-manager/programs/hyprlock.nix
@@ -1,7 +1,12 @@
+{ colorscheme, ... }:
 {
 	programs.hyprlock.settings = ''
 		general {
 			grace = 1
+		}
+
+		background {
+			path = ${./../custom/wallpapers/${colorscheme.slug}/lock}
 		}
 	'';
 }
