179023928b1d3711407317eb455008de342deadc
andrieee44 <andrieee44@gmail.com>
2024-04-25 09:43:29 +0800

    nixvim: Fix type of vim.opt.colorcolumn to string

--- modules/home-manager/programs/nixvim.nix
+++ modules/home-manager/programs/nixvim.nix
@@ -20,7 +20,7 @@
 
 				callback.__raw = ''
 					function()
-						vim.opt.colorcolumn = 76
+						vim.opt.colorcolumn = '75'
 						vim.opt.textwidth = 75
 						vim.opt.spell = true
 						vim.opt.spelllang = 'en'
