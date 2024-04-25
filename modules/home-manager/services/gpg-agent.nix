1a2a05950f22600e8d6a69fbc32efab1669764a0
andrieee44 <andrieee44@gmail.com>
2024-04-26 05:40:10 +0800

    mbsync: Add environment variables for the service

--- modules/home-manager/services/gpg-agent.nix
+++ modules/home-manager/services/gpg-agent.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
 	services.gpg-agent = {
+		defaultCacheTtl = 0;
 		extraConfig = "allow-preset-passphrase";
 		pinentryPackage = pkgs.pinentry-tty;
 	};
--- modules/home-manager/services/mbsync.nix
+++ modules/home-manager/services/mbsync.nix
@@ -1,5 +1,10 @@
-{ pkgs, ... }:
+{ config, pkgs, lib, ... }:
 {
+	systemd.user.services.mbsync.Service.Environment = lib.mkIf config.services.mbsync.enable [
+		"PASSWORD_STORE_DIR=${config.programs.password-store.settings.PASSWORD_STORE_DIR}"
+		"GNUPGHOME=${config.programs.gpg.homedir}"
+	];
+
 	services.mbsync = {
 		frequency = "*:0/15";
 		postExec = "${pkgs.notmuch}/bin/notmuch new";
