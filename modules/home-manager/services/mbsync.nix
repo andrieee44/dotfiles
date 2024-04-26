d4e764ae4d27f0229be237bc9aed38ad9f479d1e
andrieee44 <andrieee44@gmail.com>
2024-04-26 18:14:46 +0800

    mbsync: Start timer after having internet access

--- modules/home-manager/services/mbsync.nix
+++ modules/home-manager/services/mbsync.nix
@@ -1,5 +1,7 @@
 { config, pkgs, lib, ... }:
 {
+	systemd.user.timers.mbsync.Unit.After = [ "network-online.target" ];
+
 	systemd.user.services.mbsync.Service.Environment = lib.mkIf config.services.mbsync.enable [
 		"PASSWORD_STORE_DIR=${config.programs.password-store.settings.PASSWORD_STORE_DIR}"
 		"GNUPGHOME=${config.programs.gpg.homedir}"
