d913555cd50af4e2964ce3be7dc05ff9dabf5a88
andrieee44 <andrieee44@gmail.com>
2024-04-27 23:32:59 +0800

    starship: Check if enable before generating config

--- modules/home-manager/programs/starship/starship.nix
+++ modules/home-manager/programs/starship/starship.nix
@@ -1,20 +1,11 @@
 { config, lib, ... }:
-{
-	xdg.configFile = let
-		base = config.xdg.configFile."starship.toml".source;
-	in {
-		"starship/gui.toml".text = builtins.readFile base + builtins.readFile ./gui.toml;
-		"starship/tty.toml".text = builtins.readFile base + builtins.readFile ./tty.toml;
-
-		"starship.toml" = {
-			source = ./base.toml;
-			target = "starship/base.toml";
-		};
+lib.mkIf config.programs.starship.enable {
+	xdg.configFile = {
+		"starship/gui.toml".text = builtins.readFile ./base.toml + builtins.readFile ./gui.toml;
+		"starship/tty.toml".text = builtins.readFile ./base.toml + builtins.readFile ./tty.toml;
 	};
 
-	programs.zsh.initExtra = let
-		cfg = config.programs.starship;
-	in lib.mkIf cfg.enableZshIntegration ''
+	programs.zsh.initExtra = lib.mkIf config.programs.starship.enableZshIntegration ''
 		[ "$XDG_SESSION_TYPE" = "tty" ] && \
 			export STARSHIP_CONFIG="${config.home.homeDirectory}/${config.xdg.configFile."starship/tty.toml".target}" || \
 			export STARSHIP_CONFIG="${config.home.homeDirectory}/${config.xdg.configFile."starship/gui.toml".target}"
