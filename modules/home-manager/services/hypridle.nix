22cc8f453fa326c5373c3029f3dbd24029f95cd7
andrieee44 <andrieee44@gmail.com>
2024-05-01 10:18:37 +0800

    hypridle: Fix lock_cmd and reduce timers

--- modules/home-manager/services/hypridle.nix
+++ modules/home-manager/services/hypridle.nix
@@ -2,25 +2,30 @@
 {
 	services.hypridle.settings = ''
 		general {
-			lock_cmd = ${pkgs.toybox}/bin/pidof Hyprland || ${pkgs.hyprlock}/bin/hyprlock
-			unlock_cmd = ${pkgs.hyprlock}/bin/hyprlock
+			lock_cmd = ${pkgs.toybox}/bin/pidof hyprlock || ${pkgs.hyprlock}/bin/hyprlock
 			before_sleep_cmd = ${pkgs.systemd}/bin/loginctl lock-session
 			after_sleep_cmd = ${pkgs.hyprland}/bin/hyprctl dispatch dpms on
 		}
 
 		listener {
-			timeout = 300
+			timeout = 30
+			on-timeout = ${pkgs.light}/bin/light -O && ${pkgs.light}/bin/light -S 5
+			on-resume = ${pkgs.light}/bin/light -I
+		}
+
+		listener {
+			timeout = 60
 			on-timeout = ${pkgs.systemd}/bin/loginctl lock-session
 		}
 
 		listener {
-			timeout = 360
+			timeout = 90
 			on-timeout = ${pkgs.hyprland}/bin/hyprctl dispatch dpms off
 			on-resume = ${pkgs.hyprland}/bin/hyprctl dispatch dpms on
 		}
 
 		listener {
-			timeout = 420
+			timeout = 120
 			on-timeout = ${pkgs.systemd}/bin/systemctl suspend
 		}
 	'';
