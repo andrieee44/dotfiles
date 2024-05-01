fe916526e3e2bad9350146a976f1816c9fa9753e
andrieee44 <andrieee44@gmail.com>
2024-05-01 23:22:50 +0800

    direnv: Enable disable_stdin and strict_env

--- modules/home-manager/programs/direnv.nix
+++ modules/home-manager/programs/direnv.nix
@@ -1,1 +1,10 @@
-{ programs.direnv.nix-direnv.enable = true; }
+{
+	programs.direnv = {
+		nix-direnv.enable = true;
+
+		config.global = {
+			disable_stdin = true;
+			strict_env = true;
+		};
+	};
+}
