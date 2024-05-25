9769ba6c2f4b2234ab7b53643d074106eab1e8ef
andrieee44 <andrieee44@gmail.com>
2024-05-25 22:12:34 +0800

    fzf: remove color options for stylix

--- modules/home-manager/programs/fzf.nix
+++ modules/home-manager/programs/fzf.nix
@@ -1,29 +1,13 @@
-{ colorscheme, ... }:
+{ ... }:
 {
-	programs.fzf = {
-		colors = {
-			fg = "#${colorscheme.palette.base04}";
-			hl = "#${colorscheme.palette.base0D}";
-			"fg+" = "#${colorscheme.palette.base06}";
-			"bg+" = "#${colorscheme.palette.base01}";
-			info = "#${colorscheme.palette.base0A}";
-			border = "#${colorscheme.palette.base0D}";
-			prompt = "#${colorscheme.palette.base0A}";
-			pointer = "#${colorscheme.palette.base0C}";
-			marker = "#${colorscheme.palette.base0C}";
-			spinner = "#${colorscheme.palette.base0C}";
-			header = "#${colorscheme.palette.base0D}";
-		};
-
-		defaultOptions = [
-			"--exact"
-			"--reverse"
-			"--info inline"
-			"--header-first"
-			"--header 'Search file:'"
-			"--tabstop 4"
-			"--no-mouse"
-			"--preview-window border-left,wrap"
-		];
-	};
+	programs.fzf.defaultOptions = [
+		"--exact"
+		"--reverse"
+		"--info inline"
+		"--header-first"
+		"--header 'Search file:'"
+		"--tabstop 4"
+		"--no-mouse"
+		"--preview-window border-left,wrap"
+	];
 }
