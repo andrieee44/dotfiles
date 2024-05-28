70e31ec0a9110b6b92b6c918c8d6565da67ea448
andrieee44 <andrieee44@gmail.com>
2024-05-28 23:56:45 +0800

    stylix: Refactor custom console.colors to its own option

---
+++ modules/nixos/stylix/console.nix
@@ -0,0 +1,25 @@
+{ config, lib, ... }:
+{
+	options.stylix.targets.console.custom.enable = lib.mkEnableOption "andrieee44's custom implementation of stylix.targets.console";
+
+	config.console.colors = let
+		colors = config.lib.stylix.colors;
+	in lib.mkIf config.stylix.targets.console.custom.enable [
+		colors.base00
+		colors.base08
+		colors.base0B
+		colors.base0A
+		colors.base0D
+		colors.base0E
+		colors.base0C
+		colors.base05
+		colors.base03
+		colors.base08
+		colors.base0B
+		colors.base0A
+		colors.base0D
+		colors.base0E
+		colors.base0C
+		colors.base07
+	];
+}
--- modules/nixos/stylix/stylix.nix
+++ modules/nixos/stylix/stylix.nix
@@ -1,23 +1,7 @@
-{ config, lib, ... }:
+{ ... }:
 {
-	console.colors = let
-		colors = config.lib.stylix.colors;
-	in lib.mkForce [
-		colors.base00
-		colors.base08
-		colors.base0B
-		colors.base0A
-		colors.base0D
-		colors.base0E
-		colors.base0C
-		colors.base05
-		colors.base03
-		colors.base08
-		colors.base0B
-		colors.base0A
-		colors.base0D
-		colors.base0E
-		colors.base0C
-		colors.base07
-	];
+	stylix.targets.console = {
+		enable = false;
+		custom.enable = true;
+	};
 }
