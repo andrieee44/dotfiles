abec9cbe4d8e700f06af2ef9e1ef25eefa20d3f7
andrieee44 <andrieee44@gmail.com>
2024-05-28 14:13:22 +0800

    imv: Remove colorschemes

--- modules/home-manager/programs/imv.nix
+++ modules/home-manager/programs/imv.nix
@@ -1,14 +1,15 @@
-{ config, colorscheme, ... }:
+{ config, ... }:
 {
 	programs.imv.settings = {
-		options = {
+		options = let
+			colors = config.lib.stylix.colors.withHashtag;
+		in {
 			overlay = true;
 			overlay_font = "${config.gtk.font.name}:${builtins.toString config.gtk.font.size}";
 			overlay_position_bottom = true;
-
-			overlay_text_color = colorscheme.palette.base05;
-			overlay_background_color = colorscheme.palette.base00;
-			background = colorscheme.palette.base03;
+			overlay_text_color = colors.base05;
+			overlay_background_color = colors.base02;
+			background = colors.base01;
 		};
 
 		binds = {
