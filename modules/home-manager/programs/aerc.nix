fca56acffa86fb95ba11ccb7b2501466d51cf1e9
andrieee44 <andrieee44@gmail.com>
2024-05-28 20:09:28 +0800

    stylix: Move aerc styling (unfinished) and fix tmux colors in tty

--- modules/home-manager/programs/aerc.nix
+++ modules/home-manager/programs/aerc.nix
@@ -1,127 +1,18 @@
-{ config, pkgs, lib, colorscheme, ... }:
+{ config, pkgs, lib, ... }:
 {
-	programs.aerc = {
-		extraConfig = {
-			filters."text/plain" = "colorize";
-			viewer.pager = config.home.sessionVariables.PAGER;
+	programs.aerc.extraConfig = {
+		filters."text/plain" = "colorize";
+		viewer.pager = config.home.sessionVariables.PAGER;
 
-			general = {
-				term = "linux";
-				unsafe-accounts-conf = true;
-			};
-
-			ui = {
-				border-char-vertical = "│";
-				border-char-horizontal = "─";
-				styleset-name = colorscheme.slug;
-			};
+		general = {
+			term = "linux";
+			unsafe-accounts-conf = true;
 		};
 
-		stylesets = {
-			nord = ''
-				*.default=true
-				*.normal=true
-
-				default.fg = 7
-
-				error.fg = 0
-				error.bg = 1
-				error.bold = true
-
-				warning.fg = 0
-				warning.bg = 3
-				warning.bold = true
-
-				success.fg = 0
-				success.bg = 2
-				success.bold = true
-
-				title.fg = 0
-				title.bg = 6
-				title.bold = true
-
-				header.fg = 0
-				header.bg = 6
-				header.bold = true
-
-				msglist_*.selected.fg = 0
-				msglist_*.selected.bg = 6
-				msglist_*.selected.bold = true
-
-				msglist_default.fg = 7
-
-				msglist_unread.fg = 7
-				msglist_unread.bold = true
-
-				msglist_read.fg = 7
-
-				msglist_flagged.fg = 3
-				msglist_flagged.bg = 3
-
-				msglist_deleted.fg = 1
-				msglist_deleted.bg = 1
-
-				msglist_marked.fg = 5
-
-				msglist_result.fg = 7
-				msglist_result.bg = 6
-
-				msglist_answered.fg = 2
-				msglist_answered.selected.bg = 2
-
-				msglist_gutter.fg = 1
-				msglist_gutter.bg = 6
-
-				msglist_pill.fg = 1
-				msglist_pill.bg = 6
-
-				msglist_thread_folded.fg = 1
-				msglist_thread_folded.bg = 6
-
-				dirlist_*.selected.fg = 0
-				dirlist_*.selected.bg = 6
-				dirlist_*.selected.bold = true
-
-				dirlist_default.fg = 7
-
-				dirlist_unread.fg = 1
-				dirlist_unread.bold = true
-
-				dirlist_recent.fg = 1
-				dirlist_recent.bg = 6
-				dirlist_recent.bold = true
-
-				part_*.selected.fg = 0
-				part_*.selected.bg = 6
-				part_*.selected.bold = true
-
-				tab.fg = 7
-				tab.bg = 8
-				tab.selected.fg = 0
-				tab.selected.bg = 6
-				tab.selected.bold = true
-
-				stack.fg = 1
-				stack.bg = 6
-				stack.selected.fg = 1
-				stack.selected.bg = 6
-
-				border.fg = 6
-
-				statusline_*.bold = true
-
-				statusline_default.fg = 0
-				statusline_default.bg = 6
-
-				statusline_error.fg = 0
-				statusline_error.bg = 1
-
-				statusline_warning.fg = 0
-				statusline_warning.bg = 3
-
-				statusline_success.fg = 0
-				statusline_success.bg = 2
-			'';
+		ui = {
+			border-char-vertical = "│";
+			border-char-horizontal = "─";
+			styleset-name = "default";
 		};
 	};
 
--- modules/home-manager/stylix/stylix.nix
+++ modules/home-manager/stylix/stylix.nix
@@ -65,20 +65,82 @@ in {
 			guiBool = "#{!=:${"\${XDG_SESSION_TYPE}"},tty}";
 			gui = t: f: "#{?${guiBool},${t},${f}}";
 			bell = t: f: "#{?window_bell_flag,${t},${f}}";
+			normalFg = gui hashColors.base05 "white";
+			normalBg = gui hashColors.base0D "blue";
+			bellBg = gui hashColors.base08 "red";
 		in ''
-			set -g window-status-activity-style 'fg=${hashColors.base05},bg=${hashColors.base01}'
+			set -g window-status-activity-style "fg=${normalFg},bg=${hashColors.base01}"
 			set -g message-command-style 'fg=${hashColors.base06},bg=${hashColors.base02}'
 			set -g pane-border-style 'fg=${hashColors.base03}'
-			set -g pane-active-border-style 'fg=${hashColors.base0D}'
-			set -g mode-style 'fg=${hashColors.base01},bg=${hashColors.base05}'
+			set -g pane-active-border-style "fg=${normalBg}"
+			set -g mode-style "fg=${hashColors.base01},bg=${normalFg}"
 
-			set -g status-left "#[fg=${hashColors.base01},bg=${hashColors.base0D},bold] #S #[fg=${hashColors.base0D},bg=${hashColors.base01}]${gui "" " "}"
+			set -g status-left "#[fg=${hashColors.base01},bg=${normalBg},bold] #S #[fg=${normalBg},bg=${hashColors.base01}]${gui "" " "}"
 
-			set -g status-right "#[fg=${hashColors.base02},bg=${hashColors.base01}]${gui "" ""}#[fg=${hashColors.base05},bg=${hashColors.base02}] ${gui "󰥔 " ""}%b %e %Y (%a) %l:%M %p #[fg=${hashColors.base0D},bg=${hashColors.base02}]${gui "" ""}#[fg=${hashColors.base01},bg=${hashColors.base0D},bold] ${gui " " ""}#{user}@#H "
+			set -g status-right "#[fg=${hashColors.base02},bg=${hashColors.base01}]${gui "" ""}#[fg=${normalFg},bg=${hashColors.base02}] ${gui "󰥔 " ""}%b %e %Y (%a) %l:%M %p #[fg=${normalBg},bg=${hashColors.base02}]${gui "" ""}#[fg=${hashColors.base01},bg=${normalBg},bold] ${gui " " ""}#{user}@#H "
 
-			set -g window-status-current-format "#[fg=${hashColors.base01},bg=${hashColors.base0D}]${gui "" ""}#[fg=${hashColors.base01},bg=${hashColors.base0D},bold] #I ${gui "" "|"} #W #[fg=${hashColors.base0D},bg=${hashColors.base01}]${gui "" " "}"
+			set -g window-status-current-format "#[fg=${hashColors.base01},bg=${normalBg}]${gui "" ""}#[fg=${hashColors.base01},bg=${normalBg},bold] #I ${gui "" "|"} #W #[fg=${normalBg},bg=${hashColors.base01}]${gui "" " "}"
 
-			set -g window-status-format "#[fg=${hashColors.base01},bg=${bell hashColors.base08 hashColors.base02}]${gui "" ""}#[fg=${bell hashColors.base01 hashColors.base05},bg=${bell "${hashColors.base08}#,bold" "${hashColors.base02}#,nobold"}] #I ${gui "" "|"} #W #[fg=${bell hashColors.base08 hashColors.base02},bg=${hashColors.base01}]${gui "" " "}"
+			set -g window-status-format "#[fg=${hashColors.base01},bg=${bell bellBg hashColors.base02}]${gui "" ""}#[fg=${bell hashColors.base01 normalFg},bg=${bell "${bellBg}#,bold" "${hashColors.base02}#,nobold"}] #I ${gui "" "|"} #W #[fg=${bell bellBg hashColors.base02},bg=${hashColors.base01}]${gui "" " "}"
+		'';
+
+		aerc.stylesets.default = ''
+			*.default=true
+
+			default.bg=#20212b
+
+			title.reverse=true
+			header.bold=true
+			header.fg=#8be9fd
+
+			*error.bold=true
+			error.fg=#ff5555
+			warning.fg=#f1fa8c
+			success.fg=#50fa7b
+
+			statusline*.default=true
+			statusline_default.reverse=true
+			statusline_error.fg=#ff5555
+			statusline_error.reverse=true
+			statusline_default.fg=#303030
+			statusline_default.bg=#af87ff
+
+			dirlist_default.selected.fg=#f8f8f2
+			dirlist_default.selected.bg=#44475a
+			dirlist_recent.selected.fg=#44475a
+			dirlist_recent.selected.bg=#f8f8f2
+			dirlist_unread.fg=#50fa7b
+			dirlist_unread.selected.fg=#50fa7b
+			dirlist_unread.selected.bg=#44475a
+
+			msglist_default.selected.fg=#44475a
+			msglist_default.selected.bg=#f8f8f2
+			msglist_unread.bold=true
+			msglist_unread.fg=#50fa7b
+			msglist_unread.selected.bg=#44475a
+			msglist_read.selected.fg=#f8f8f2
+			msglist_read.selected.bg=#44475a
+			msglist_marked.fg=#f1fa8c
+			msglist_marked.selected.fg=#f1fa8c
+			msglist_marked.selected.bg=#44475a
+			msglist_deleted.fg=#ff5555
+			msglist_result.fg=#8be9fd
+			msglist_result.selected.bg=#44475a
+
+			msglist_deleted.selected.reverse=toggle
+
+			completion_pill.reverse=true
+
+			tab.reverse=true
+			border.reverse = true
+			tab.bg=#9c7adf
+			tab.fg=#303030
+			tab.selected.bg=#303030
+			tab.selected.fg=#9c7adf
+			border.fg=#20212b
+
+			selector_focused.reverse=true
+			selector_chooser.bold=true
 		'';
 	};
 
