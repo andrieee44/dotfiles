c6fecd20490d481e311ba3d91e694028513212cf
andrieee44 <andrieee44@gmail.com>
2024-05-29 11:20:49 +0800

    stylix: Refactor mpv

---
+++ modules/home-manager/stylix/mpv.nix
@@ -0,0 +1,8 @@
+{ config, lib, ... }:
+{
+	options.stylix.targets.custom.mpv.enable = lib.mkEnableOption "custom implementation of styling mpv";
+
+	config.programs.mpv.scriptOpts.uosc.color = let
+		colors = config.lib.stylix.colors;
+	in lib.mkIf config.stylix.targets.custom.mpv.enable "foreground=${colors.base05},foreground_text=${colors.base01},background=${colors.base02},background_text=${colors.base05},curtain=${colors.base03},success=${colors.base0B},error=${colors.base08}";
+}
--- modules/home-manager/stylix/stylix.nix
+++ modules/home-manager/stylix/stylix.nix
@@ -1,18 +1,18 @@
-{ config, ... }:
-let
-	colors = config.lib.stylix.colors;
-in {
-	programs = {
-		mpv.scriptOpts.uosc.color = "foreground=${colors.base05},foreground_text=${colors.base01},background=${colors.base02},background_text=${colors.base05},curtain=${colors.base03},success=${colors.base0B},error=${colors.base08}";
-	};
+{ ... }:
+{
 
 	stylix.targets = {
 		firefox.profileNames = [ "default" ];
+		tmux.enable = false;
+		fzf.enable = false;
 
 		custom = {
 			aerc.enable = true;
 			imv.enable = true;
+			mpv.enable = true;
 			zsh.enable = true;
+			tmux.enable = true;
+			fzf.enable = true;
 
 			nixvim = {
 				enable = true;
@@ -27,13 +27,7 @@ in {
 				enable = true;
 				background_alpha = 0.5;
 			};
-
-			tmux.enable = true;
-			fzf.enable = true;
 		};
-
-		tmux.enable = false;
-		fzf.enable = false;
 
 		nixvim.transparent_bg = {
 			main = true;
