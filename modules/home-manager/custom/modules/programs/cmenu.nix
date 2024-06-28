7fd2f7eb0856ec67f4f608b7b1cc5baef68547ab
andrieee44 <andrieee44@gmail.com>
2024-06-28 22:35:53 +0800

    jsonstatus,cmenu: Remove default package

--- modules/home-manager/custom/modules/programs/cmenu.nix
+++ modules/home-manager/custom/modules/programs/cmenu.nix
@@ -1,22 +1,8 @@
-{ config, pkgs, lib, ... }:
+{ config, lib, ... }:
 {
 	options.custom.programs.cmenu = {
 		enable = lib.mkEnableOption "cmenu";
-
-		package = lib.mkOption {
-			type = lib.types.package;
-			default = pkgs.buildGoModule {
-				name = "cmenu";
-				vendorHash = null;
-
-				src = pkgs.fetchFromGitHub {
-					owner = "andrieee44";
-					repo = "cmenu";
-					rev = "39535fa5dc6cc88435d7b6d572e37fecba8949d4";
-					hash = "sha256-estt7CFQOyrPYHjLhcJs4IbFSYtFIOBBV7L5mXP4rak=";
-				};
-			};
-		};
+		package = lib.mkOption { type = lib.types.package; };
 	};
 
 	config.home.packages = let
--- modules/home-manager/custom/modules/programs/jsonstatus.nix
+++ modules/home-manager/custom/modules/programs/jsonstatus.nix
@@ -5,21 +5,7 @@ in {
 	options.custom.programs.jsonstatus = {
 		enable = lib.mkEnableOption "jsonstatus";
 		settings = lib.mkOption { type = lib.types.attrsOf toml.type; };
-
-		package = lib.mkOption {
-			type = lib.types.package;
-			default = pkgs.buildGoModule {
-				name = "jsonstatus";
-				vendorHash = "sha256-7jlGdnOoeVVG7qp5InYnTrwxDHvOVJapiybGgopIjYE=";
-
-				src = pkgs.fetchFromGitHub {
-					owner = "andrieee44";
-					repo = "jsonstatus";
-					rev = "006154cf37fe972965f7c91b39e591c1c2f7b497";
-					hash = "sha256-ESnb0QZAF9QQPDnXMwWqMzJkq/AtiamJ3dn+6hQ4FyY=";
-				};
-			};
-		};
+		package = lib.mkOption { type = lib.types.package; };
 	};
 
 	config = let
