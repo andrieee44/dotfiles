9ad9e9b14f823b21aa6f308586c2165bfc277d4c
andrieee44 <andrieee44@gmail.com>
2024-06-30 02:38:49 +0800

    cmenu: Update

--- modules/home-manager/custom/programs/cmenu.nix
+++ modules/home-manager/custom/programs/cmenu.nix
@@ -8,8 +8,8 @@
 			src = pkgs.fetchFromGitHub {
 				owner = "andrieee44";
 				repo = "cmenu";
-				rev = "9cf508979cdb4d0a55c4ee75fb1e5366afd6ae53";
-				hash = "sha256-epXzk7JVSybGPGqzpG3y6/2urblMWC9L+dgaUy6uy7s=";
+				rev = "72a0fe0bc183ad4c1ca563d0be1267ad9eb6c197";
+				hash = "sha256-4+/oIlnvD/TnXYIRCVLtm/Wdhbidsx0Z60XEioSP43Q=";
 			};
 		};
 	};
