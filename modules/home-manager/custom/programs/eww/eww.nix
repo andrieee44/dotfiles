42ba16c592d885562fdbb52d7ad043efd6e2baed
andrieee44 <andrieee44@gmail.com>
2024-08-07 23:05:43 +0800

    Fix wrong jstat path

--- modules/home-manager/custom/programs/eww/eww.nix
+++ modules/home-manager/custom/programs/eww/eww.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 {
 	custom.programs.eww.yuck = builtins.readFile (pkgs.runCommand "eww.yuck" {} ''
-		substitute "${./eww.yuck}" "$out" --replace-fail "jstat" "${config.custom.programs.jsonstatus.package}/bin/jstat"
+		substitute "${./eww.yuck}" "$out" --replace-fail "jstat" "${config.custom.programs.jstat.package}/bin/jstat"
 	'');
 }
