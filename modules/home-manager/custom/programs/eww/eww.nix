3b4f1e103683d5efaca60078a46ce7d5977a882c
andrieee44 <andrieee44@gmail.com>
2024-08-07 23:03:53 +0800

    Use jstat instead of jsonstatus

--- modules/home-manager/custom/programs/eww/eww.nix
+++ modules/home-manager/custom/programs/eww/eww.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 {
 	custom.programs.eww.yuck = builtins.readFile (pkgs.runCommand "eww.yuck" {} ''
-		substitute "${./eww.yuck}" "$out" --replace-fail "jsonstatus" "${config.custom.programs.jsonstatus.package}/bin/jsonstatus"
+		substitute "${./eww.yuck}" "$out" --replace-fail "jstat" "${config.custom.programs.jsonstatus.package}/bin/jstat"
 	'');
 }
--- modules/home-manager/custom/programs/jstat.nix
+++ modules/home-manager/custom/programs/jstat.nix
@@ -1,15 +1,16 @@
 { pkgs, ... }:
 {
-	custom.programs.jstat.package = pkgs.buildGoModule rec {
-		name = "jstat";
-		vendorHash = "sha256-a0lx9owbPGOMjh5lGygaAczJOjVP15p9aV82QYz9990=";
-		sourcePath = "${src.name}/cmd/jstat";
-
+	custom.programs.jstat.package = let
 		src = pkgs.fetchFromGitHub {
 			owner = "andrieee44";
 			repo = "jstat";
-			rev = "4175e09871d4b7d0ec2bc979ce283a02e03b1e7f";
-			hash = "sha256-N8j8i3stV9/ZdrtOkvawxxVm40HVw7liadQGQSZtJxA=";
+			rev = "ba5f1f8794b4e1151da57df89a0cfbb86d7fd977";
+			hash = "sha256-EphrJ2Gx4zW5RqC+gdnaffp4jZwlkozWQOR/ItMk4wk=";
 		};
+	in pkgs.buildGoModule {
+		name = "jstat";
+		vendorHash = "sha256-fKQUVEwpHZPX3Wp1s3//U7fvLoDeY2Nt6EyPr+EUFME=";
+		sourcePath = "${src.name}/cmd/jstat";
+		src = src;
 	};
 }
