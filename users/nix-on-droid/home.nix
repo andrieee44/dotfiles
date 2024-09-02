ba5bff7ace10271eca460c6a605d887db16cac65
andrieee44 <andrieee44@gmail.com>
2024-09-03 05:18:49 +0800

    Update home.nix

--- users/nix-on-droid/home.nix
+++ users/nix-on-droid/home.nix
@@ -1,8 +1,9 @@
-{ config, pkgs, ... }:
+{ config, pkgs, stateVersion, ... }:
 {
 	home = {
 		username = "nix-on-droid";
 		homeDirectory = "/data/data/com.termux.nix/files/home";
+		stateVersion = stateVersion;
 
 		sessionVariables.SSH_ASKPASS = pkgs.writers.writeDash "ssh_askpass" ''
 			set -eu
