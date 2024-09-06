019a4cb96c47a3b443282f0df19e1af1e562ebe9
andrieee44 <andrieee44@gmail.com>
2024-09-07 07:14:43 +0800

    Update stylix.nix

--- modules/stylix/stylix.nix
+++ modules/stylix/stylix.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
 	stylix = {
-		enable = true;
+		enable = false;
 		image = ./wallpaper;
 		base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-storm.yaml";
 		polarity = "dark";
