ad45283aab3d36a1d23824284fe9b1a76ccb8031
andrieee44 <andrieee44@gmail.com>
2024-09-19 20:09:53 +0800

    Add tview settings

--- modules/home-manager/custom/programs/tview.nix
+++ modules/home-manager/custom/programs/tview.nix
@@ -8,11 +8,123 @@
 			src = pkgs.fetchFromGitHub {
 				owner = "andrieee44";
 				repo = "tview";
-				rev = "e8e4927970fd8857fb2817a1ef282845fea28b57";
-				hash = "sha256-QxKL4+YZOj+srEXBxXn6UUaGHs1fyAjF4igPQH4mfPk=";
+				rev = "4367b59a0820a6e3e93fb31c1e8f76eea5dd2bd8";
+				hash = "sha256-ZLMDAKabUSMrq1isO9yRGjMjATRhKaoEg46kfLCCjI0=";
 			};
 		};
 
-		settings = {};
+		settings = let
+			audioVideo =  [ ''${pkgs.mediainfo}/bin/mediainfo -- "$TVIEW_FILE"'' ];
+			archive =  [ ''${pkgs.atool}/bin/atool -l -- "$TVIEW_FILE"'' ];
+			office =  [ ''${pkgs.libreoffice}/bin/libreoffice --cat "$TVIEW_FILE"'' ];
+
+			image = [
+				''${pkgs.chafa}/bin/chafa -s "$[TVIEW_WIDTH];x${"\${TVIEW_HEIGHT}"}" -- "$TVIEW_FILE"''
+				''${pkgs.mediainfo}/bin/mediainfo -- "$TVIEW_FILE"''
+			];
+
+			jq = [
+				''${pkgs.jaq}/bin/jaq --color always . "$TVIEW_FILE"''
+				''${pkgs.jq}/bin/jq -C . "$TVIEW_FILE"''
+			];
+
+			text = [
+				''${pkgs.bat}/bin/bat --color always --paging never --termiinal-width "$TVIEW_WIDTH" -- "$TVIEW_FILE"''
+				''${pkgs.highlight}/bin/highlight --force -- "$TVIEW_FILE"''
+				''${pkgs.sourceHighlight}/bin/source-highlight --failsafe -i "$TVIEW_FILE"''
+				''${pkgs.toybox}/bin/cat -- "$TVIEW_FILE"''
+			];
+
+			html = [
+				''${pkgs.elinks}/bin/elinks -dump 1 -no-references -no-numbering -dump-width "$TVIEW_WIDTH" "$TVIEW_FILE"''
+				''${pkgs.lynx}/bin/lynx -dump -nonumbers -nolist -width "$TVIEW_WIDTH" -- "$TVIEW_FILE"''
+				''${pkgs.w3m}/bin/w3m -dump "$TVIEW_FILE"''
+			];
+
+			diff = [
+				''${pkgs.delta}/bin/delta < "$TVIEW_FILE"''
+				''${pkgs.diff-so-fancy}/bin/diff-so-fancy < "$TVIEW_FILE"''
+				''${pkgs.colordiff}/bin/colordiff < "$TVIEW_FILE"''
+			];
+		in {
+			"audio/aac" = audioVideo;
+			"application/x-abiword" = office;
+			"image/apng" = image;
+			"application/x-freearc" = archive;
+			"image/avif" = image;
+			"video/x-msvideo" = audioVideo;
+			"image/bmp" = image;
+			"application/x-bzip" = archive;
+			"application/x-bzip2" = archive;
+			"application/x-cdf" = audioVideo;
+			"application/x-csh" = text;
+			"text/css" = text;
+			"text/csv" = text;
+			"application/msword" = office;
+			"application/vnd.openxmlformats-officedocument.wordprocessingml.document" = office;
+			"application/gzip" = archive;
+			"application/x-gzip" = archive;
+			"image/gif" = image;
+			"text/html" = html;
+			"image/vnd.microsoft.icon" = image;
+			"application/java-archive" = archive;
+			"image/jpeg" = image;
+			"text/javascript" = text;
+			"application/json" = jq;
+			"application/ld+json" = jq;
+			"audio/midi" = audioVideo;
+			"audio/x-midi" = audioVideo;
+			"audio/mpeg" = audioVideo;
+			"video/mp4" = audioVideo;
+			"video/mpeg" = audioVideo;
+			"application/vnd.oasis.opendocument.presentation" = office;
+			"application/vnd.oasis.opendocument.spreadsheet" = office;
+			"application/vnd.oasis.opendocument.text" = office;
+			"audio/ogg" = audioVideo;
+			"video/ogg" = audioVideo;
+			"application/ogg" = audioVideo;
+			"image/png" = image;
+			"application/pdf" = office;
+			"application/x-httpd-php" = text;
+			"application/vnd.ms-powerpoint" = office;
+			"application/vnd.openxmlformats-officedocument.presentationml.presentation" = office;
+			"application/vnd.rar" = archive;
+			"application/rtf" = text;
+			"application/x-sh" = text;
+			"image/svg+xml" = image;
+			"application/x-tar" = archive;
+			"image/tiff" = image;
+			"video/mp2t" = audioVideo;
+			"text/plain" = text;
+			"audio/wav" = audioVideo;
+			"audio/webm" = audioVideo;
+			"video/webm" = audioVideo;
+			"image/webp" = image;
+			"application/xhtml+xml" = html;
+			"application/vnd.ms-excel" = office;
+			"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" = office;
+			"application/xml" = text;
+			"application/zip" = archive;
+			"video/3gpp" = audioVideo;
+			"audio/3gpp" = audioVideo;
+			"video/3gpp2" = audioVideo;
+			"audio/3gpp2" = audioVideo;
+			"application/x-7z-compressed" = archive;
+			"text/x-diff" = diff;
+			"text/x-patch" = diff;
+			"application/x-bittorrent" = [ ''${pkgs.transmission}/bin/transmission-show -- "$TVIEW_FILE"'' ];
+			"inode/directory" = [ ''${pkgs.coreutils}/bin/ls --color --group-directories-first -w "$TVIEW_WIDTH" -- "$TVIEW_FILE"'' ];
+
+			"application/octet-stream" = [
+				''${pkgs.exiftool}/bin/exiftool -- "$TVIEW_FILE"''
+				''${pkgs.file}/bin/file -- "$TVIEW_FILE"''
+				''${pkgs.toybox}/bin/cat -- "$TVIEW_FILE"''
+			];
+
+			"text/markdown" = [
+				''${pkgs.glow}/bin/glow -w "$TVIEW_WIDTH" -- "$TVIEW_FILE"''
+				''${pkgs.mdcat}/bin/mdcat --columns "$TVIEW_WIDTH" -- "$TVIEW_FILE"''
+			];
+		};
 	};
 }
