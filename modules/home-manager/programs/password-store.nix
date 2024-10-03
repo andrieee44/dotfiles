c4f4b9067bf3b7e8431d1baab2b0445521656b53
andrieee44 <andrieee44@gmail.com>
2024-10-03 08:16:40 +0800

    password-store: Add pass-otp for GitHub 2FA

--- modules/home-manager/programs/password-store.nix
+++ modules/home-manager/programs/password-store.nix
@@ -1,11 +1,15 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
-	programs.password-store.settings = let
-		dir = "${config.xdg.dataHome}/password-store";
-	in {
-		PASSWORD_STORE_DIR = dir;
-		PASSWORD_STORE_GENERATED_LENGTH = "30";
-		PASSWORD_STORE_ENABLE_EXTENSIONS = "true";
-		PASSWORD_STORE_EXTENSIONS_DIR = "${dir}/extensions";
+	programs.password-store = {
+		package = pkgs.pass.withExtensions (exts: [ exts.pass-otp ]);
+
+		settings = let
+			dir = "${config.xdg.dataHome}/password-store";
+		in {
+			PASSWORD_STORE_DIR = dir;
+			PASSWORD_STORE_GENERATED_LENGTH = "30";
+			PASSWORD_STORE_ENABLE_EXTENSIONS = "true";
+			PASSWORD_STORE_EXTENSIONS_DIR = "${dir}/extensions";
+		};
 	};
 }
