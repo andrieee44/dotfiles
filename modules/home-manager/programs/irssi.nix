71713c7dd275c6c580510a6f029b8e862d6c80a9
andrieee44 <andrieee44@gmail.com>
2024-10-08 20:40:24 +0800

    irssi: Adjust some options

--- modules/home-manager/programs/irssi.nix
+++ modules/home-manager/programs/irssi.nix
@@ -24,6 +24,15 @@
 				dcc_download_path = "${config.xdg.userDirs.download}";
 				dcc_upload_path = "${config.xdg.userDirs.publicShare}";
 			};
+
+			lookandfeel = {
+				timestamp_format = "%l:%M %p";
+				timestamp_format_alt = "%b %e %Y (%a) %l:%M %p";
+			};
+
+			misc = {
+				settings_autosave = "no";
+			};
 		'';
 	};
 
