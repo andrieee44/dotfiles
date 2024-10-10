f49c5d227ecad593c14d3ba5fff1ce41bbef185c
andrieee44 <andrieee44@gmail.com>
2024-10-10 21:26:01 +0800

    starship: Add fallback value for XDG_SESSION_TYPE

--- modules/home-manager/programs/starship/starship.nix
+++ modules/home-manager/programs/starship/starship.nix
@@ -6,7 +6,7 @@ lib.mkIf config.programs.starship.enable {
 	};
 
 	programs.zsh.initExtra = lib.mkIf config.programs.starship.enableZshIntegration ''
-		[ "$XDG_SESSION_TYPE" = "tty" ] && \
+		[ "${"\${XDG_SESSION_TYPE:-}"}" = "tty" ] && \
 			export STARSHIP_CONFIG="${config.home.homeDirectory}/${config.xdg.configFile."starship/tty.toml".target}" || \
 			export STARSHIP_CONFIG="${config.home.homeDirectory}/${config.xdg.configFile."starship/gui.toml".target}"
 	'';
