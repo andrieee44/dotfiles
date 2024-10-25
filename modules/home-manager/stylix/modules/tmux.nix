b24d949d0a250566c20f4664bc37284ccce53efe
nix-on-droid <andrieee44@gmail.com>
2024-10-25 23:09:33 +0800

    stylix: Use alternative colors in tmux tty

--- modules/home-manager/stylix/modules/tmux.nix
+++ modules/home-manager/stylix/modules/tmux.nix
@@ -8,17 +8,17 @@
 		gui = t: f: "#{?${guiBool},${t},${f}}";
 		bell = t: f: "#{?window_bell_flag,${t},${f}}";
 	in lib.mkIf config.stylix.targets.custom.tmux.enable ''
-		set -g status-style 'fg=${colors.base05},bg=${colors.base00}'
-		set -g window-status-bell-style 'fg=${colors.base00},bg=${colors.base08}'
-		set -g pane-border-style 'fg=${colors.base03}'
-		set -g pane-active-border-style 'fg=${colors.base0D}'
-		set -g mode-style 'fg=${colors.base00},bg=${colors.base05}'
-		set -g message-style 'fg=${colors.base05},bg=${colors.base00}'
-		set -g message-command-style 'fg=${colors.base05},bg=${colors.base03}'
-		set -g clock-mode-colour '${colors.base0D}'
-		set -g status-left "#[fg=${colors.base00},bg=${colors.base0D},bold] #S #[fg=${colors.base0D},bg=${colors.base00}]${gui "" " "}"
-		set -g status-right "#[fg=${colors.base03},bg=${colors.base00}]${gui "" ""}#[fg=${colors.base05},bg=${colors.base03}] ${gui "󰥔 " ""}%b %e %Y (%a) %l:%M %p #[fg=${colors.base0D},bg=${colors.base03}]${gui "" ""}#[fg=${colors.base00},bg=${colors.base0D},bold] ${gui " " ""}#{user}@#H "
-		set -g window-status-current-format "#[fg=${colors.base00},bg=${colors.base0D}]${gui "" ""}#[fg=${colors.base00},bg=${colors.base0D},bold] #I ${gui "" "|"} #W #[fg=${colors.base0D},bg=${colors.base00}]${gui "" " "}"
-		set -g window-status-format "#[fg=${colors.base00},bg=${bell "${colors.base08}" "${colors.base03}"}]${gui "" ""}#[fg=${bell "${colors.base00}" "${colors.base05}"},bg=${bell "${colors.base08}#,bold" "${colors.base03}#,nobold"}] #I ${gui "" "|"} #W #[fg=${bell "${colors.base08}" "${colors.base03}"},bg=${colors.base00}]${gui "" " "}"
+		set -g status-style "fg=${gui "#${colors.base05}" "white"},bg=${gui "#${colors.base00}" "black"}"
+		set -g window-status-bell-style "fg=${gui "#${colors.base00}" "black"},bg=${gui "#${colors.base08}" "red"}"
+		set -g pane-border-style "fg=${gui "#${colors.base03}" "brightblack"}"
+		set -g pane-active-border-style "fg=${gui "#${colors.base0D}" "blue"}"
+		set -g mode-style "fg=${gui "#${colors.base00}" "black"},bg=${gui "#${colors.base05}" "white"}"
+		set -g message-style "fg=${gui "#${colors.base05}" "white"},bg=${gui "#${colors.base00}" "black"}"
+		set -g message-command-style "fg=${gui "#${colors.base05}" "white"},bg=${gui "#${colors.base03}" "brightblack"}"
+		set -Fg clock-mode-colour "${gui "#${colors.base0D}" "blue"}"
+		set -g status-left "#[fg=${gui "#${colors.base00}" "black"},bg=${gui "#${colors.base0D}" "blue"},bold] #S #[fg=${gui "#${colors.base0D}" "blue"},bg=${gui "#${colors.base00}" "black"}]${gui "" " "}"
+		set -g status-right "#[fg=${gui "#${colors.base03}" "brightblack"},bg=${gui "#${colors.base00}" "black"}]${gui "" ""}#[fg=${gui "#${colors.base05}" "white"},bg=${gui "#${colors.base03}" "brightblack"}] ${gui "󰥔 " ""}%b %e %Y (%a) %l:%M %p #[fg=${gui "#${colors.base0D}" "blue"},bg=${gui "#${colors.base03}" "brightblack"}]${gui "" ""}#[fg=${gui "#${colors.base00}" "black"},bg=${gui "#${colors.base0D}" "blue"},bold] ${gui " " ""}#{user}@#H "
+		set -g window-status-current-format "#[fg=${gui "#${colors.base00}" "black"},bg=${gui "#${colors.base0D}" "blue"}]${gui "" ""}#[fg=${gui "#${colors.base00}" "black"},bg=${gui "#${colors.base0D}" "blue"},bold] #I ${gui "" "|"} #W #[fg=${gui "#${colors.base0D}" "blue"},bg=${gui "#${colors.base00}" "black"}]${gui "" " "}"
+		set -g window-status-format "#[fg=${gui "#${colors.base00}" "black"},bg=${bell "#${colors.base08}" "${colors.base03}"}]${gui "" ""}#[fg=${bell "#${gui "#${colors.base00}" "black"}" "${gui "#${colors.base05}" "white"}"},bg=${bell "#${colors.base08}#,bold" "${colors.base03}#,nobold"}] #I ${gui "" "|"} #W #[fg=${bell "#${colors.base08}" "${colors.base03}"},bg=${gui "#${colors.base00}" "black"}]${gui "" " "}"
 	'';
 }
