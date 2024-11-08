c03d74427e10f872a6da9ad3673022c0be33ad5e
andrieee44 <andrieee44@gmail.com>
2024-11-09 00:03:41 +0800

    nixvim: Add more sources

--- modules/home-manager/programs/nixvim.nix
+++ modules/home-manager/programs/nixvim.nix
@@ -117,11 +117,16 @@
     extraConfigLua = ''
       vim.opt.termguicolors = vim.env.XDG_SESSION_TYPE ~= 'tty'
 
-      local signs = {
-      	Error = vim.opt.termguicolors._value and ' ' or '! ',
-      	Warn = vim.opt.termguicolors._value and ' ' or '? ',
-      	Hint = vim.opt.termguicolors._value and ' ' or '* ',
-      	Info = vim.opt.termguicolors._value and ' ' or 'i ',
+      local signs = vim.opt.termguicolors._value and {
+      	Error = ' ',
+      	Warn = ' ',
+      	Hint = ' ',
+      	Info = ' ',
+      } or {
+      	Error = '! ',
+      	Warn = '? ',
+      	Hint = '* ',
+      	Info = 'i ',
       }
 
       for k, v in pairs(signs) do
@@ -159,26 +164,30 @@
     '';
 
     plugins = {
-      cmp-buffer.enable = true;
+      cmp-buffer.enable = false;
       cmp-calc.enable = true;
       cmp-cmdline.enable = true;
       cmp-cmdline-history.enable = true;
       cmp-emoji.enable = true;
+      cmp-fuzzy-buffer.enable = true;
+      cmp-fuzzy-path.enable = true;
       cmp-git.enable = true;
-      cmp_luasnip.enable = true;
+      cmp-latex-symbols.enable = true;
       cmp-nvim-lsp-document-symbol.enable = true;
       cmp-nvim-lsp.enable = true;
       cmp-nvim-lsp-signature-help.enable = true;
       cmp-nvim-lua.enable = true;
       cmp-nvim-ultisnips.enable = true;
-      cmp-path.enable = true;
+      cmp-path.enable = false;
       cmp-tmux.enable = true;
       cmp-treesitter.enable = true;
       cmp-vsnip.enable = true;
-      cmp_yanky.enable = true;
       cmp-zsh.enable = true;
+      cmp_luasnip.enable = true;
+      cmp_yanky.enable = true;
       lsp-format.enable = true;
       lsp-lines.enable = true;
+      lsp-signature.enable = true;
       luasnip.enable = true;
 
       cmp =
@@ -194,6 +203,8 @@
 
           filetype.gitcommit.sources = [
             (source plugins.cmp-buffer "buffer" { })
+            (source plugins.cmp-fuzzy-buffer "fuzzy_buffer" { })
+            (source plugins.cmp-fuzzy-path "fuzzy_path" { })
             (source plugins.cmp-git "git" { })
           ];
 
@@ -252,6 +263,7 @@
               (source plugins.cmp-buffer "buffer" { })
               (source plugins.cmp-calc "calc" { })
               (source plugins.cmp-emoji "emoji" { })
+              (source plugins.cmp-latex-symbols "latex_symbols" { })
               (source plugins.cmp_luasnip "luasnip" { })
               (source plugins.cmp-nvim-lsp "nvim_lsp" { })
               (source plugins.cmp-nvim-lsp-signature-help "nvim_lsp_signature_help" { })
@@ -299,7 +311,10 @@
                 nix_on_droid.expr = ''import (builtins.getFlake "github:andrieee44/dotfiles").nixOnDroidConfigurations.nix-on-droid.options'';
               };
 
-              formatting.command = [ "${pkgs.nixfmt-rfc-style}/bin/nixfmt -s" ];
+              formatting.command = [
+                "${pkgs.writers.writeDash "nixfmtStrict" "set -eu; ${pkgs.nixfmt-rfc-style}/bin/nixfmt -s \"$@\""}"
+              ];
+
               nixpkgs.expr = ''import (builtins.getFlake "github:andrieee44/dotfiles").inputs.nixpkgs {}'';
             };
           };
