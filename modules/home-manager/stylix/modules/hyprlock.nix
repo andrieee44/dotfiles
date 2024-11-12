8802facecd89ada6a9ece24fd7d5da36e459c6eb
andrieee44 <andrieee44@gmail.com>
2024-11-12 19:02:58 +0800

    stylix: Convert path into string for hyprlock wallpaper

--- modules/home-manager/stylix/modules/hyprlock.nix
+++ modules/home-manager/stylix/modules/hyprlock.nix
@@ -3,6 +3,6 @@
   options.stylix.targets.custom.hyprlock.enable = lib.mkEnableOption "custom implementation of styling hyprlock";
 
   config.programs.hyprlock.settings = lib.mkIf config.stylix.targets.custom.hyprlock.enable {
-    background = [ { path = config.stylix.image; } ];
+    background = [ { path = builtins.toString config.stylix.image; } ];
   };
 }
