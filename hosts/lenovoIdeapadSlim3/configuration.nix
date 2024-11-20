ab2b72a5b4f54940f2e161daf749eb7781d1ae02
andrieee44 <andrieee44@gmail.com>
2024-11-20 20:18:21 +0800

    Reduce builder accounts to one

--- hosts/lenovoIdeapadSlim3/configuration.nix
+++ hosts/lenovoIdeapadSlim3/configuration.nix
@@ -1,44 +1,38 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 {
   imports = [ ./hardware-configuration.nix ];
   networking.hostName = "lenovoIdeapadSlim3";
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
-  nix.settings.trusted-users = [ "@builders" ];
+  nix.settings.trusted-users = [ "builder" ];
 
   users = {
-    groups.builders = { };
+    users = {
+      builder = {
+        createHome = true;
+        isNormalUser = true;
+        shell = pkgs.zsh;
 
-    users =
-      let
-        builder = {
-          createHome = true;
-          group = "builders";
-          isNormalUser = true;
-          shell = pkgs.zsh;
-
-          openssh.authorizedKeys.keys = [
-            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDJtUQ3XeEdvcpvQp4DkLPiskGmNzW4+STtNOM1k4aGZ nix-on-droid@oppoReno8Z"
-          ];
-        };
-      in
-      (lib.genAttrs (builtins.map (x: "builder${builtins.toString x}") (lib.range 1 2)) (key: builder))
-      // {
-        andrieee44 = {
-          createHome = true;
-          group = "users";
-          isNormalUser = true;
-          shell = pkgs.zsh;
-
-          extraGroups = [
-            "audio"
-            "floppy"
-            "input"
-            "networkmanager"
-            "render"
-            "video"
-            "wheel"
-          ];
-        };
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDJtUQ3XeEdvcpvQp4DkLPiskGmNzW4+STtNOM1k4aGZ nix-on-droid@oppoReno8Z"
+        ];
       };
+
+      andrieee44 = {
+        createHome = true;
+        group = "users";
+        isNormalUser = true;
+        shell = pkgs.zsh;
+
+        extraGroups = [
+          "audio"
+          "floppy"
+          "input"
+          "networkmanager"
+          "render"
+          "video"
+          "wheel"
+        ];
+      };
+    };
   };
 }
--- hosts/nix-on-droid/nix-on-droid.nix
+++ hosts/nix-on-droid/nix-on-droid.nix
@@ -1,11 +1,6 @@
+{ pkgs, stateVersion, ... }:
 {
-  pkgs,
-  lib,
-  stateVersion,
-  ...
-}:
-{
-  time.timeZone = "Asia/Manila";
+  time.timeZonZ = "Asia/Manila";
   system.stateVersion = stateVersion;
   user.shell = "${pkgs.zsh}/bin/zsh";
 
@@ -13,24 +8,15 @@
     pkgs.nerdfonts.override { fonts = [ "SourceCodePro" ]; }
   }/share/fonts/truetype/NerdFonts/SauceCodeProNerdFontMono-Regular.ttf";
 
-  nix =
-    let
-      builders = builtins.map (x: "ssh-ng://builder${builtins.toString x}@192.168.100.7") (lib.range 1 2);
-    in
-    {
-      substituters = builders;
+  nix = {
+    trustedPublicKeys = [ "builder@lenovoIdeapadSlim3:FhBevAZRvgSE05PQ0FFw2kIuOLqhlNbtM+JcUwjvnK0=" ];
 
-      trustedPublicKeys = [
-        "builder1@lenovoIdeapadSlim3:FhBevAZRvgSE05PQ0FFw2kIuOLqhlNbtM+JcUwjvnK0="
-        "builder2@lenovoIdeapadSlim3:WLWNV313efm+HRaHuF7hNLpESgxUIVmUXeI2Zpeb1sM="
-      ];
-
-      extraOptions = ''
-        builders-use-substitutes = true
-        experimental-features = nix-command flakes
-        builders = ${builtins.concatStringsSep " " builders}
-      '';
-    };
+    extraOptions = ''
+      builders-use-substitutes = true
+      experimental-features = nix-command flakes
+      builders = ssh-ng://builder@192.168.100.7
+    '';
+  };
 
   environment = {
     motd = "";
