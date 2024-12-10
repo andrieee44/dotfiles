895fdb7b60d110766aa3c55407f5637fb94c5b8d
andrieee44 <andrieee44@gmail.com>
2024-12-10 20:47:26 +0800

    treefmt: Add nixfmt

--- modules/treefmt/treefmt.nix
+++ modules/treefmt/treefmt.nix
@@ -1,7 +1,15 @@
+{ pkgs, ... }:
 {
   projectRootFile = "flake.nix";
 
   programs = {
-    nixfmt.enable = true;
+    nixfmt = {
+      enable = true;
+      package = pkgs.nixfmt-rfc-style;
+    };
+  };
+
+  settings.formatter = {
+    nixfmt.options = [ "-s" ];
   };
 }
