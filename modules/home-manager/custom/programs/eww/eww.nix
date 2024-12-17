2b15acaa2c76cc67e28957f79ae041bcef60378b
andrieee44 <andrieee44@gmail.com>
2024-12-17 16:44:37 +0800

    eww: Remove lock.yuck

--- modules/home-manager/custom/programs/eww/eww.nix
+++ modules/home-manager/custom/programs/eww/eww.nix
@@ -3,7 +3,6 @@
   custom.programs.eww.yuck = builtins.readFile (
     pkgs.runCommand "eww.yuck" { } ''
         substitute "${./eww.yuck}" "$out" \
-      		--replace "lock.yuck" "${./lock.yuck}" \
       		--replace "jstat" "${config.custom.programs.jstat.package}/bin/jstat" \
       		--replace "hyprctl" "${config.wayland.windowManager.hyprland.package}/bin/hyprctl"
     ''
