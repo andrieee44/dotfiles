c61a9e70a042975fbd0cff84d7cc93b89b6fb617
nix-on-droid <andrieee44@gmail.com>
2025-01-21 12:40:07 +0800

    password-store: Move extensions dir

--- modules/home-manager/programs/password-store.nix
+++ modules/home-manager/programs/password-store.nix
@@ -11,7 +11,7 @@
         PASSWORD_STORE_DIR = dir;
         PASSWORD_STORE_GENERATED_LENGTH = "30";
         PASSWORD_STORE_ENABLE_EXTENSIONS = "true";
-        PASSWORD_STORE_EXTENSIONS_DIR = "${dir}/extensions";
+        PASSWORD_STORE_EXTENSIONS_DIR = "${dir}/.extensions";
       };
   };
 }
