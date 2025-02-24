4d7b57966222efd1c8ac4cbb439fa130b8671511
andrieee44 <andrieee44@gmail.com>
2025-02-24 19:13:47 +0800

    eww: Fix restart

--- modules/home-manager/custom/modules/programs/eww.nix
+++ modules/home-manager/custom/modules/programs/eww.nix
@@ -60,7 +60,6 @@
           {
             ExecStart = "${eww} daemon --no-daemonize";
             ExecStop = "${eww} kill";
-            ExecReload = "${eww} reload";
             ExecStartPost = "${pkgs.uwsm}/bin/uwsm app -- ${pkgs.eww}/bin/eww open bar --screen 0";
           };
       };
