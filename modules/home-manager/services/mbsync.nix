638d42fae9ea5b10b8aee176fd2f07403ee4b9d7
andrieee44 <andrieee44@gmail.com>
2025-02-26 19:55:50 +0800

    mbsync: Fix notification not appearing

--- modules/home-manager/services/mbsync.nix
+++ modules/home-manager/services/mbsync.nix
@@ -16,6 +16,7 @@
     postExec = "-${pkgs.writers.writeDash "postdelivery" ''
       set -eu
 
+      ${pkgs.mu}/bin/mu index
       unread="$(${pkgs.mu}/bin/mu find 'flag:unread' | ${pkgs.toybox}/bin/wc -l)"
       ${pkgs.notmuch}/bin/notmuch new
       [ "$unread" -ne 0 ] && ${pkgs.notify-desktop}/bin/notify-desktop -a "mbsync"  "You have ${"\${unread}"} unread emails"
