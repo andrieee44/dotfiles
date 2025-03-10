201ccc19657534c6fe226790fd79d2e1dcbd0de5
andrieee44 <andrieee44@gmail.com>
2025-03-10 22:17:01 +0800

    mangohud: Move HUD to middle-top

--- modules/home-manager/programs/mangohud.nix
+++ modules/home-manager/programs/mangohud.nix
@@ -30,7 +30,7 @@
       hud_no_margin = false;
       no_small_font = true;
       permit_upload = false;
-      position = "top";
+      position = "middle-top";
       present_mode = true;
       ram = true;
       resolution = true;
