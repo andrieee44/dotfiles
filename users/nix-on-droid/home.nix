48c64c06bf90b4e1ade0cf8f355614971286f196
nix-on-droid <andrieee44@gmail.com>
2025-03-25 05:28:42 +0800

    nix-on-droid: Disable bash

--- users/nix-on-droid/home.nix
+++ users/nix-on-droid/home.nix
@@ -8,13 +8,17 @@
   custom.programs.tview.enable = lib.mkForce false;
   dconf.enable = false;
 
-  programs.zsh.initExtra =
-    let
-      ssh = "${config.programs.ssh.package}/bin";
-    in
-    ''
-      eval "$(${ssh}/ssh-agent -s)" > /dev/null && ${ssh}/ssh-add > /dev/null 2>& 1
-    '';
+  programs = {
+    bash.enable = lib.mkForce false;
+
+    zsh.initExtra =
+      let
+        ssh = "${config.programs.ssh.package}/bin";
+      in
+      ''
+        eval "$(${ssh}/ssh-agent -s)" > /dev/null && ${ssh}/ssh-add > /dev/null 2>& 1
+      '';
+  };
 
   home = {
     username = "nix-on-droid";
